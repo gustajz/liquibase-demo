e2399094ac92d8dd212200ee6127caf44905d420
Gustavo Jotz <gustavo@jotz.com.br>
2016-07-07 01:21:40 -0300

    exemplo de utilizacao do liquibase

---
+++ initdb-prd.sql
@@ -0,0 +1,5 @@
+-- demo user
+CREATE USER demo WITH PASSWORD 'prd123';
+
+-- demo database 
+CREATE DATABASE demo WITH OWNER = demo; 
---
+++ initdb.sql
@@ -0,0 +1,5 @@
+-- demo user
+CREATE USER demo WITH PASSWORD 'demo123';
+
+-- demo database 
+CREATE DATABASE demo WITH OWNER = demo; 
